bdc74979072b514a1a5af047faf07f401cd1da9c
Flavio Doi <flavioydoi@edu.unifil.br>
2024-03-20 18:17:13 -0300

    Primeiro Commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+package cadastroava3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+	      int numRegistros;
+
+	      System.out.println("Quantos registros deseja cadastrar? ");
+	      numRegistros = sc.nextInt();
+	      sc.nextLine();
+
+
+	}
+
+}
